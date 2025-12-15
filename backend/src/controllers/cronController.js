e2728e982249ce5cffb6184e9f3542458bb1988f
Fred Schultz <schultzfred8@gmail.com>
2025-12-15 17:37:21 -0600

    changes

--- backend/src/controllers/cronController.js
+++ backend/src/controllers/cronController.js
@@ -2,7 +2,7 @@ import { pool } from "../config/db.js";
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
+  process.env.DATABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
