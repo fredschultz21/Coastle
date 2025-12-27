f550693df8f4b24763c7518d5e44824177cf8aca
Fred Schultz <schultzfred8@gmail.com>
2025-12-27 13:52:19 -0600

    changes

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -30,7 +30,7 @@ export default function Home() {
   const IMAGE_WIDTH = 300;
   const IMAGE_HEIGHT = 225;
   const MAP_OFFSET_X = -10;
-  const MAP_OFFSET_Y = 3.5;
+  const MAP_OFFSET_Y = 5;
   const MAP_WIDTH = 590;
   const MAP_HEIGHT = 417;
 
