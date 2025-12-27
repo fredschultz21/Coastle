6396da95009e930d6fd90b84e6d57e83ed2c1a33
Fred Schultz <schultzfred8@gmail.com>
2025-12-27 13:01:51 -0600

    changes

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -73,7 +73,7 @@ export default function Home() {
 
     let yNorm = mapY / imgHeight;
 
-    yNorm = 0.72 * yNorm + 0.08;
+    yNorm = 0.71 * yNorm + 0.01;
 
     const mercN = Math.PI * (1 - 2 * yNorm);
     let lat = (180 / Math.PI) * Math.atan(Math.sinh(mercN));
